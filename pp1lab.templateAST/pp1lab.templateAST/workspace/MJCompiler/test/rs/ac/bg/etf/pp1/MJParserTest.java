c2609b7eff5f2d5eda82afdbae75a96ecaa9879a
Veljko Djorovic <73741659+veljodj@users.noreply.github.com>
2021-04-09 10:26:06 +0200

    Prepravljen MJParserTest.java, kreiran CodeGenerator.java, izmenjen build.xml i nije pokrenuto nista, krecemo polako na pravljenje CodeGeneratora

--- pp1lab.templateAST/pp1lab.templateAST/workspace/MJCompiler/test/rs/ac/bg/etf/pp1/MJParserTest.java
+++ pp1lab.templateAST/pp1lab.templateAST/workspace/MJCompiler/test/rs/ac/bg/etf/pp1/MJParserTest.java
@@ -3,6 +3,7 @@ package rs.ac.bg.etf.pp1;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
@@ -14,6 +15,7 @@ import org.apache.log4j.xml.DOMConfigurator;
 
 import rs.ac.bg.etf.pp1.ast.Program;
 import rs.ac.bg.etf.pp1.util.Log4JUtils;
+import rs.etf.pp1.mj.runtime.Code;
 import rs.etf.pp1.symboltable.Tab;
 
 public class MJParserTest {
@@ -58,7 +60,15 @@ public class MJParserTest {
 			Tab.dump();
 
 			if (!p.errorDetected && v.passed()) {
-				log.info("Parsiranje uspesno zavrseno");
+				File objFile = new File("test/program.obj");
+				if(objFile.exists()) objFile.delete();
+				
+				CodeGenerator codeGenerator = new CodeGenerator();
+				prog.traverseBottomUp(codeGenerator);
+				Code.dataSize = v.nVars;
+				Code.mainPc = codeGenerator.getMainPc();
+				Code.write(new FileOutputStream(objFile));
+				log.info("Parsiranje uspesno zavrseno!");
 			} else {
 				log.error("Parsiranje NIJE uspesno zavrseno");
 			}

3b56c9fe2cc0cfaff9671e190b3ab0d27df7c823
Veljko Djorovic <73741659+veljodj@users.noreply.github.com>
2021-04-09 14:06:10 +0200

    U build.xml dodat disasm i runObj i nije pokretan. Ovo ce nam omoguciti pokretanje samog koda na ljudima vidljivom nacinu, kao i pokretanje programa i pracenje ispisa koji nastaje na osnovu generisanog Bajtokad

--- pp1lab.templateAST/pp1lab.templateAST/workspace/MJCompiler/src/rs/ac/bg/etf/pp1/CodeGenerator.java
+++ pp1lab.templateAST/pp1lab.templateAST/workspace/MJCompiler/src/rs/ac/bg/etf/pp1/CodeGenerator.java
@@ -1,6 +1,10 @@
 package rs.ac.bg.etf.pp1;
 
+import rs.ac.bg.etf.pp1.ast.StatPrintValue;
 import rs.ac.bg.etf.pp1.ast.VisitorAdaptor;
+import rs.etf.pp1.mj.runtime.Code;
+import rs.etf.pp1.symboltable.Tab;
+import rs.etf.pp1.symboltable.concepts.Obj;
 
 public class CodeGenerator extends VisitorAdaptor {
 
@@ -9,4 +13,25 @@ public class CodeGenerator extends VisitorAdaptor {
 	public int getMainPc() {
 		return mainPc;
 	}
+	
+	@Override
+	public void visit(StatPrintValue StatPrintValue) {
+		if(StatPrintValue.getExpr().struct == Tab.intType){
+			Code.loadConst(5);
+			Code.put(Code.print);
+		}else{
+			Code.loadConst(1);
+			Code.put(Code.bprint);
+		}
+	}
+	
+	public void visit(rs.ac.bg.etf.pp1.ast.ConstDeclOneElementNumber ConstDeclOneElementNumber){
+		Obj con = Tab.insert(Obj.Con, "$", ConstDeclOneElementNumber.struct);
+		con.setLevel(0);
+		con.setAdr(ConstDeclOneElementNumber.getVal()); 
+		
+		Code.load(con);
+	}
+	
+	
 }
